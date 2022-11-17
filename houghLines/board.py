10654df63a94cc15b8ec933c6d893b22316842b4
impri2 <impri@naver.com>
2022-11-18 00:33:56 +0900

    Update board.py
    add comments

--- houghLines/board.py
+++ houghLines/board.py
@@ -15,15 +15,15 @@ for filename,image,label in zip(filenames,images,labels):
     dist = []
     for i in range(len(lines)):
         for rho,theta in lines[i]:
-            thetas.append(theta)
+            thetas.append(theta) # Hough transform으로 구한 각도 List
     for i in range(len(lines)):
         dist.append([])
         for j in range(len(lines)):
-            dist[i].append(angle_distance(thetas[i],thetas[j]))
+            dist[i].append(angle_distance(thetas[i],thetas[j])) #각도 List의 모든 pairwise distance를 구함
     print(thetas)
-    cluster = AgglomerativeClustering(n_clusters=2,affinity='precomputed',linkage='average')
+    cluster = AgglomerativeClustering(n_clusters=2,affinity='precomputed',linkage='average') 
     cluster.fit(dist)
-    print(cluster.labels_)
+    print(cluster.labels_) #cluster에 따라 0/1 이 저장됨
     for i in range(len(lines)):
         for rho, theta in lines[i]:
             a = math.cos(theta)
@@ -32,5 +32,5 @@ for filename,image,label in zip(filenames,images,labels):
             y0 = b * rho
             pt1 = (int(x0 + 1000*(-b)), int(y0 + 1000*(a)))
             pt2 = (int(x0 - 1000*(-b)), int(y0 - 1000*(a)))
-            cv2.line(image, pt1, pt2, (0,0,255) if cluster.labels_[i] else (255,255,0), 1, cv2.LINE_AA)
+            cv2.line(image, pt1, pt2, (0,0,255) if cluster.labels_[i] else (255,255,0), 1, cv2.LINE_AA) #cluster label의 값 0/1에 따라 색을 다르게 함
     cv2.imwrite(os.path.join('hough',filename[-4:]+'.png'),image)

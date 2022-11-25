cfbd96287c116f1a7fef41fd0a1ccf615537014c
impri2 <impri@naver.com>
2022-11-25 22:27:22 +0900

    Update main.py

--- houghLines/main.py
+++ houghLines/main.py
@@ -3,31 +3,34 @@ from dataset import load_rendered_images
 import cv2
 import os
 import glob
-for file in glob.glob('homography/*.png'):
-  image = cv2.imread(file)
-  xmin,xmax,ymin,ymax = get_board(image=image,xmax=2,ymax=2)
-  print(xmin,xmax,ymin,ymax)
-  greyImage = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
-  edge = cv2.Canny(greyImage,140,150)
-  cv2.imwrite( os.path.join('canny',file[-8:]),edge)
-  cv2.circle(image,(640,640),5,(255,0,255),2,)
+from skimage import filters
+import numpy as np
+from skimage import img_as_ubyte
+
+
   
-  cv2.line(image,(80*ymin+640,80*xmin+640),(80*ymax+640,80*xmin+640),(255,0,255),2)
-  cv2.line(image,(80*ymin+640,80*xmax+640),(80*ymax+640,80*xmax+640),(255,0,255),2)
-  cv2.line(image,(80*ymax+640,80*xmax+640),(80*ymax+640,80*xmin+640),(255,0,255),2)
-  cv2.line(image,(80*ymin+640,80*xmin+640),(80*ymin+640,80*xmax+640),(255,0,255),2)
-  cv2.imwrite( os.path.join('board',file[-8:]),image)
-  
-'''if __name__ == '__main__':
+if __name__ == '__main__':
     filenames,images,labels = load_rendered_images()
+    
     for filename,image,label in zip(filenames,images,labels):
-      #image = get_homography_from_image(image)
-      #image = draw_lines(image)
+      image,xmax,ymax = get_homography_from_image(image)
+      
       cv2.imwrite(os.path.join('homography',filename[-4:]+'.png'),image)
-'''
+      
+      xmin,xmax,ymin,ymax=get_board(image,xmax,ymax)
+      cv2.line(image,(80*ymin+640,80*xmin+640),(80*ymax+640,80*xmin+640),(255,0,255),2)
+      cv2.line(image,(80*ymin+640,80*xmax+640),(80*ymax+640,80*xmax+640),(255,0,255),2)
+      cv2.line(image,(80*ymax+640,80*xmax+640),(80*ymax+640,80*xmin+640),(255,0,255),2)
+      cv2.line(image,(80*ymin+640,80*xmin+640),(80*ymin+640,80*xmax+640),(255,0,255),2)
+      cv2.imwrite( os.path.join('board',filename[-4:]+'.png'),image)
+
 '''
 dataset.py: 데이터셋 불러오는 코드
 board.py: 이미지 처리하는 코드
 board.py, dataset.py,utils.py, main.py가 있는 폴더에 train폴더가 있고, 그 안에 있는 train폴더에 xxxx.png, xxxx.json데이터가 있다.
+같은 폴더 내에 homography 폴더, board 폴더를 만들어 두고 실행해야 함.
+homography matrix 찾을 때 반복 횟수는 board.py 49번 줄의 수를 바꾸면 된다.
+
+
 필요 라이브러리: cv2, scikit-learn, numpy, sympy
 '''

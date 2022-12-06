201ca573965b5a4819aa622d163c6379a2fb858c
cdh981009 <cdh981009@gmail.com>
2022-12-07 02:28:26 +0900

    Optimize homography computing phase

--- houghLines/board.py
+++ houghLines/board.py
@@ -16,15 +16,18 @@ def get_intersections(lines1,lines2):
             coordinates.append(get_intersection(line1[0][0],line1[0][1],line2[0][0],line2[0][1]))
     return coordinates
 
+# Using lines1, cluster similar lines in lines2 into one
 def merge_lines(lines1,lines2):
-    #lines2를 merge하는 함수
     intersections=[]
     mergedLines=[]
+
     for line in lines2: #lines1의 임의의 선을 골라 그 선과의 교점을 구함(논문에서는 lines1의 평균을 냄)
         intersections.append(get_intersection(lines1[0][0][0],lines1[0][0][1],line[0][0],line[0][1]))
+
     cluster = DBSCAN(eps=20,min_samples=2)
     cluster.fit(intersections)#교점과 가까운 것기리 merge됨
-    check = set() 
+    check = set()
+
     for i in range(len(lines2)):
         if cluster.labels_[i]==-1:
             mergedLines.append(lines2[i])
@@ -47,57 +50,63 @@ def get_homography(lines1, lines2, gamma=0.02):
     
     lines1.sort(key=lambda line:math.cos(line[0][1])/line[0][0])
     lines2.sort(key=lambda line:math.cos(line[0][1])/line[0][0])# 선 정렬
-    
+
+    max_cell_size = 5
+
     for X in range(len(lines1)-1):# 정렬순으로 선택하기
-     for Y in range(len(lines2)-1):
-        chosenLines1 = lines1[X:X+2]
-        chosenLines2 = lines2[Y:Y+2]
-        line_intersection = []
-        for line1 in chosenLines1:# 선택한 선의 교점
-            for line2 in chosenLines2:
-                line_intersection.append(get_intersection(line1[0][0],line1[0][1],line2[0][0],line2[0][1]))
+        for Y in range(len(lines2)-1):
+            chosenLines1 = lines1[X:X+2]
+            chosenLines2 = lines2[Y:Y+2]
+            line_intersection = []
+            for line1 in chosenLines1:# 선택한 선의 교점
+                for line2 in chosenLines2:
+                    line_intersection.append(get_intersection(line1[0][0],line1[0][1],line2[0][0],line2[0][1]))
 
-        for sx in range(1,9):
-            for sy in range(1,9): #구한 교점이 이루는 사각형이 1x1, 1x2 ... 8x8크기라고 가정하고 homography를 구함
-                inliers=[]
-                inliers_warped=[]
+            # 구한 교점이 이루는 사각형이 1x1, 1x2 ... max_cell_size x max_cell_size
+            # 크기라고 가정하고 homography를 구함
+            for sx in range(1, max_cell_size + 1):
+                for sy in range(1, max_cell_size + 1):
+                    inliers=[]
+                    inliers_warped=[]
 
-                homography = get_homography_from_four_coordinates(line_intersection,sx,sy)
+                    homography = get_homography_from_four_coordinates(line_intersection,sx,sy)
 
-                for i in range(N): #모든 교점에 대해 warp
-                    warped= homography @ np.array([[intersections[i][0]],[intersections[i][1]],[1]])
-                    warped/=warped[2]
+                    for i in range(N): #모든 교점에 대해 warp
+                        warped= homography @ np.array([[intersections[i][0]],[intersections[i][1]],[1]])
+                        warped/=warped[2]
 
-                    dist = math.hypot(warped[0][0]-round(warped[0][0]),warped[1][0]-round(warped[1][0]))#한 칸이 1x1크기가 되도록 warp 했으므로 inlier는 좌표가 정수에 가까워야 함
-                    if dist<gamma: #가장 가까운 정수좌표로부터 거리가 gamma 이하라면 inlier에 추가
-                        inliers.append(intersections[i])
-                        inliers_warped.append((round(warped[0][0]),round(warped[1][0])))
+                        dist = math.hypot(warped[0][0]-round(warped[0][0]),warped[1][0]-round(warped[1][0]))#한 칸이 1x1크기가 되도록 warp 했으므로 inlier는 좌표가 정수에 가까워야 함
+                        if dist<gamma: #가장 가까운 정수좌표로부터 거리가 gamma 이하라면 inlier에 추가
+                            inliers.append(intersections[i])
+                            inliers_warped.append((round(warped[0][0]),round(warped[1][0])))
 
-                if len(inliers)>len(max_inlier_set):
-                    max_inlier_set = inliers
-                    max_inlier_warped = inliers_warped
-                #최대 inlier수가 N/2에 도달할 때까지 반복
+                    if len(inliers)>len(max_inlier_set):
+                        max_inlier_set = inliers
+                        max_inlier_warped = inliers_warped
+                    #최대 inlier수가 N/2에 도달할 때까지 반복
+                    if len(max_inlier_set)>N//2:
+                         break
                 if len(max_inlier_set)>N//2:
-                     break
+                    break
             if len(max_inlier_set)>N//2:
                 break
-        if len(max_inlier_set)>N//2:
-            break
 
     iter = 150
-    for x in range(iter):#may be changed
-        if len(max_inlier_set)>N//2:
+    for x in range(iter):
+        if len(max_inlier_set) > N//2:
             break
 
         #임의로 가로선 2개, 세로선 2개 선택
         chosenLines1 = random.sample(lines1,2)
         chosenLines2 = random.sample(lines2,2)
         line_intersection = []
+
         for line1 in chosenLines1:# 선택한 선의 교점
             for line2 in chosenLines2:
                 line_intersection.append(get_intersection(line1[0][0],line1[0][1],line2[0][0],line2[0][1]))
-        for sx in range(1,9):
-            for sy in range(1,9): #구한 교점이 이루는 사각형이 1x1, 1x2 ... 8x8크기라고 가정하고 homography를 구함
+
+        for sx in range(1, max_cell_size + 1):
+            for sy in range(1, max_cell_size + 1): #구한 교점이 이루는 사각형이 1x1, 1x2 ... 8x8크기라고 가정하고 homography를 구함
                 inliers=[]
                 inliers_warped=[]
                 
@@ -124,16 +133,21 @@ def get_homography(lines1, lines2, gamma=0.02):
         if len(max_inlier_set)>N//2:
             break
 
-    xmin,xmax,ymin,ymax = min([i[0] for i in max_inlier_warped]),max([i[0] for i in max_inlier_warped]),min([i[1] for i in max_inlier_warped]),max([i[1] for i in max_inlier_warped])
+    xmin, xmax, ymin, ymax = min([i[0] for i in max_inlier_warped]),\
+                             max([i[0] for i in max_inlier_warped]),\
+                             min([i[1] for i in max_inlier_warped]),\
+                             max([i[1] for i in max_inlier_warped])
+
     for i in range(len(max_inlier_warped)):
-       max_inlier_warped[i]=( max_inlier_warped[i][0]-(xmin),  max_inlier_warped[i][1]-(ymin))#xmin, ymin을 0으로 함
-    xmax-=xmin
-    ymax-=ymin
+        max_inlier_warped[i]=( max_inlier_warped[i][0]-(xmin),  max_inlier_warped[i][1]-(ymin)) # xmin, ymin을 0으로 함
+
+    xmax -= xmin
+    ymax -= ymin
     # print(xmax,ymax)
 
     homography = cv2.findHomography(np.array(max_inlier_set,dtype=np.float32),80*np.array(max_inlier_warped,dtype=np.float32)+640)[0]
    
-    return homography,xmax,ymax
+    return homography, xmax, ymax
 
 def get_lines(image, debug=False):
     img_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
@@ -232,12 +246,13 @@ def draw_lines(image, lines1, lines2):
 # xmax and ymax is right-bottom corner coordinate for naively detected board
 def get_homography_from_image(image, debug=False):
     lines = get_lines(image, debug=debug)
+
     lines1, lines2 = cluster_lines(lines)
     lines2 = merge_lines(lines1,lines2)
     lines1 = merge_lines(lines2,lines1)
 
     if debug:
-        open_wait_cv2_window("lines", draw_lines(image, lines1, lines2))
+        open_wait_cv2_window("lines", draw_lines(image.copy(), lines1, lines2))
 
     homography, xmax, ymax = get_homography(lines1, lines2)
 
